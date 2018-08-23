9afbb0ca1c7f0bb6f58bab7606d74d625082c894
冯浩 <302917797@qq.com>
2018-08-23 12:25:00 +0800

    新建忘记密码activity

---
+++ app/src/main/java/com/big/fishcash/cash/ui/activity/ForgetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.big.fishcash.cash.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.big.fishcash.cash.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_activity_forget_password);
+    }
+}
